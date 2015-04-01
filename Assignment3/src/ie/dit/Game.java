1bfcec567edfa620158063b0f78bb8d87bd2e596
Ronakss <C10717975@mydit.ie>
2015-04-01 23:09:59 +0100

    small change

--- Assignment3/src/ie/dit/Game.java
+++ Assignment3/src/ie/dit/Game.java
@@ -6,15 +6,18 @@ public class Game {
 	
 	PApplet applet; // We need this to access our main PApplet.
 	
+	Map map;
 	
 	public Game(PApplet applet){
 		this.applet = applet;
 		this.applet.size(1000, 1000);
 		this.applet.smooth();
-		
+		map = new map(this);
+				
 	} // End constructor.
 	
 	public void run(){
+		map.run();
 		
 	} // End run.
 	
