5674c681e3d0266598ab7a81125dd73435f3da1b
DamienGlynn <damienglynn@me.com>
2015-04-01 16:06:50 +0100

    GameObject Updated.

--- Assignment3/src/ie/dit/GameObject.java
+++ Assignment3/src/ie/dit/GameObject.java
@@ -1,25 +1,43 @@
 package ie.dit;
 
-// The base class.
+import java.awt.Color;
+import processing.core.PApplet;
+import processing.core.PVector;
+
 public class GameObject {
 	
-	float theta;
+	public PApplet applet;
+	public float w, h, theta;
+	public PVector location, velocity;
+	public Color colour;
 	
-	GameObject(){
+	public GameObject(PApplet applet) {
+		this.applet = applet;
+		w = h = 100.0f;
 		theta = 0.0f;
+		location = new PVector(500.0f, 500.0f);
+		velocity = new PVector(0.0f, 0.0f);
+		colour   = new Color(255, 0, 0);
 	} // End constructor.
 	
-	public void run() {
+	public GameObject(float w, float h, PVector location, PVector velocity, Color colour, PApplet applet) {
+		this.applet = applet;
+		this.w = w;
+		this.h = h;
+		this.location = location;
+		this.velocity = velocity;
+		this.colour = colour;
+	} // End constructor.
+	
+	public void run(){
 		display();
 		update();
 	} // End run.
 	
 	public void display() {
-		
 	} // End display.
-
-	public void update() {
 	
+	public void update() {
 	} // End update.
-
-} // End GameObject.
+	
+} // End GameObject class.
