1af3617168ca15b2fc41a11ea505ff8b49ef4981
ScottDIT <scottlynchccp@gmail.com>
2015-04-08 19:13:18 +0100

    Bounds

--- Assignment3/src/ie/dit/Player.java
+++ Assignment3/src/ie/dit/Player.java
@@ -12,7 +12,7 @@ public class Player extends Helicopter {
 		
 		location = new PVector ( (applet.width/2)-(w/2), applet.height-(h*2) );//Location of player
 		velocity = new PVector (0.0f, 10.0f);
-		up = down = left = right = false; //Set all to false for keypress
+		up = down = left = right = false; //Set all to false for Keypress
 		
 		
 	} // End Constructor
@@ -42,8 +42,34 @@ public class Player extends Helicopter {
 		if(up) location.sub(velocity);
 		if(down) location.add(velocity);
 		if(left) theta -= 0.1;
+		//if(left) location.x -= 0.1;
+		//if (theta > -0.5) {
+		//theta -= 0.025;
+       // }
 		if(right) theta += 0.1;
 
 	} // End move
+	
+	void InBounds(Helicopter player) 
+	{
+		if (player.location.y > 600) {//Bottom bound
+			player.location.y =  50;
+			
+			
+			
+	  } else 
+		  if (player.location.y >= h-100)//Bottom bound
+			  {
+			  player.location.y = h-100;
+			  } else
+		  if (player.location.x <= player.w/2) //Left bound
+		  {
+		    player.location.x =  player.w/2;
+		  } else 
+		    if (player.location.x >= w-(player.w/2)-10)//Right bound
+		  {
+		    player.location.x = w-(player.w/2)-10;
+		  }
+	} // End inBounds
 	
 } // End Class
