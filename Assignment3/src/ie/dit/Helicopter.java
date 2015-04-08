8b84ea11d6a14a5591ef5ddf5218c6476a084522
DamienGlynn <damienglynn@me.com>
2015-04-08 21:22:25 +0100

    Player movement updated, comments added.

--- Assignment3/src/ie/dit/Helicopter.java
+++ Assignment3/src/ie/dit/Helicopter.java
@@ -12,45 +12,41 @@ public class Helicopter extends GameObject {
 		w = 30;
 		h = 40;
 		rotor = 0.0f;
-		location = new PVector(300,300);
-		
+		location = new PVector(300,300);	
 	} // End Constructor.
-	
-	
 	
 	public void display() {
 		applet.noStroke();
-		applet.fill( colour.getRed(), colour.getGreen(), colour.getBlue(), colour.getAlpha() );
-		
-		applet.pushMatrix();
-		applet.translate(location.x  + (w/2), location.y  + (h/2));
-		applet.rotate(theta);
-		
-		applet.rect(-3,h-20, 6, h-20); // Tail.
-		
-		//applet.rect(-5, h/2, 40, 3); // Crossbar
-		applet.rect(-15, h, 30, 3);
-		applet.ellipse(0,0, w, h);
-		applet.popMatrix();
-		
-		applet.pushMatrix();
-		applet.translate(location.x  + (w/2), location.y  + (h/2));
-		applet.rotate(rotor);
-		applet.fill(0);
-		applet.rect( -1.5f, -40, 3, 80); // Vertical rotor
-		applet.rect( -40, -1.5f, 80, 3); // Horizontal rotor
-		applet.popMatrix();
-		
+		draw_helicopter();
+		draw_rotor();
 		applet.noFill();
-
-		
-		
-	} //End Display.
-	
-	
+	} // End Display.
 	
 	public void update() {
 		rotor += 0.5f;
 	} // End update.
 	
-} //End Helicopter Class.
+	private void draw_helicopter() {
+		// Draw the helicopter.
+		applet.fill( colour.getRed(), colour.getGreen(), colour.getBlue(), colour.getAlpha() );
+		applet.pushMatrix();
+		applet.translate(location.x  + (w/2), location.y  + (h/2));
+		applet.rotate(theta);
+		applet.rect(-15, h, 30, 3);    // Tail fin.
+		applet.rect(-3,h-20, 6, h-20); // Tail.
+		applet.ellipse(0,0, w, h);     // Body.
+		applet.popMatrix();
+	} // End draw_helicopter.
+	
+	private void draw_rotor() {
+		// Draw the rotor.
+		applet.pushMatrix();
+		applet.translate(location.x  + (w/2), location.y  + (h/2));
+		applet.rotate(rotor);
+		applet.fill(0);
+		applet.rect(-1.5f, -40, 3, 80); // Vertical rotor.
+		applet.rect(-40, -1.5f, 80, 3); // Horizontal rotor.
+		applet.popMatrix();
+	} // End draw_rotor.
+
+} // End Helicopter Class.
--- Assignment3/src/ie/dit/Player.java
+++ Assignment3/src/ie/dit/Player.java
@@ -4,72 +4,65 @@ import processing.core.PApplet;
 import processing.core.PVector;
 
 public class Player extends Helicopter {
-
+	
+	PVector forward;
 	boolean up, down, left, right;
 	
 	Player(PApplet applet) {
 		super(applet);
-		
-		location = new PVector ( (applet.width/2)-(w/2), applet.height-(h*2) );//Location of player
-		velocity = new PVector (0.0f, 10.0f);
-		up = down = left = right = false; //Set all to false for Keypress
-		
-		
-	} // End Constructor
+		location = new PVector ( (applet.width/2)-(w/2), applet.height-(h*2) );
+		velocity = new PVector (0.0f, 5.0f);
+		forward = new PVector (0.0f, 1.0f);
+		up = down = left = right = false; // Set to true on KeyPressed.
+	} // End constructor.
 	
 	public void update() {
+		// By calling super.update(), we are calling the Helicopter
+		// classes update method which rotates the helicopter rotor.
 		super.update();
 		move();
+		inBounds();
 	} // End update
 	
 	public void keyPressed(){
-		setDirection(applet.keyCode,true);
+		setDirection(applet.keyCode, true);
 	} // End keyPressed.
 	
 	public void keyReleased(){
-		setDirection(applet.keyCode,false);
-
+		setDirection(applet.keyCode, false);
 	} // End keyPressed.
 	
 	private void setDirection(int k, boolean decision) {
-		if     (k == 87 || k == 38) up = decision; //87 & 38 represent keypress code
-		else if(k == 83 || k == 40) down = decision;
-		else if(k == 65 || k == 37) left = decision;
-		else if(k == 68 || k == 39) right = decision;
+		if     (k == 87 || k == 38) up    = decision; // 87=w || 38=arrow-up
+		else if(k == 83 || k == 40) down  = decision; // 83=s || 40=arrow-down
+		else if(k == 65 || k == 37) left  = decision; // 65=a || 37=arrow-left
+		else if(k == 68 || k == 39) right = decision; // 68=d || 39=arrow-right
 	} // End setDirection.
 	
 	private void move() {
-		if(up) location.sub(velocity);
-		if(down) location.add(velocity);
-		if(left) theta -= 0.1;
-		//if(left) location.x -= 0.1;
-		//if (theta > -0.5) {
-		//theta -= 0.025;
-       // }
+		// This will make the player move in the direction it is facing.
+		forward.x = (float) -Math.sin(theta) * velocity.y;
+		forward.y = (float)  Math.cos(theta) * velocity.y;
+		// These booleans are set to true when the keys are pressed.
+		if(up)    location.sub(forward);
+		if(down)  location.add(forward);
+		if(left)  theta -= 0.1;
 		if(right) theta += 0.1;
-
-	} // End move
+	} // End move.
 	
-	void InBounds(Helicopter player) 
-	{
-		if (player.location.y > 600) {//Bottom bound
-			player.location.y =  50;
-			
-			
-			
-	  } else 
-		  if (player.location.y >= h-100)//Bottom bound
-			  {
-			  player.location.y = h-100;
-			  } else
-		  if (player.location.x <= player.w/2) //Left bound
-		  {
-		    player.location.x =  player.w/2;
-		  } else 
-		    if (player.location.x >= w-(player.w/2)-10)//Right bound
-		  {
-		    player.location.x = w-(player.w/2)-10;
-		  }
-	} // End inBounds
+	void inBounds() {
+		// If the player passes the bottom of the screen.
+		if (location.y > 700)
+			location.y = -60; // Put the player at the top of the screen.
+		// If the player passes the top of the screen.
+		if (location.y < -60)
+			location.y = 700; // Put the player at the bottom of the screen.
+		// If the player passes the left side of the screen.
+		if (location.x < -40)
+			location.x = 720; // Put the player at the right side of the screen.
+		// If the player passes the right side of the screen.
+		if (location.x > 720)
+			location.x = -40; // Put the player at the left side of the screen.
+	} // End inBounds.
 	
-} // End Class
+} // End Player class.
