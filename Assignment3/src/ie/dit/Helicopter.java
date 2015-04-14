a1d034d8fd4478a2358f2835ef2b63bbb08c5965
Salim Uddin <salimuddin_01@hotmail.com>
2015-04-14 14:41:45 +0100

    Helicopter class added

--- Assignment3/src/ie/dit/Helicopter.java
+++ Assignment3/src/ie/dit/Helicopter.java
@@ -1,19 +1,42 @@
 package ie.dit;
 
+import ddf.minim.Minim;
+import ddf.minim.AudioPlayer;
+import java.util.ArrayList;
 import processing.core.PApplet;
 import processing.core.PVector;
 
 public class Helicopter extends GameObject {
-
-	float rotor;
+	
+	public int health;
+	public PVector forward;
+	public ArrayList<GameObject> objects;
+	public float rotor_theta, shadow_scale, fireRate = 1f, timeSinceLastEvent = 0;
+	public Minim minim; // Required to use Minim.
+	public AudioPlayer snd_shoot;
 	
 	Helicopter(PApplet applet) {
 		super(applet);
 		w = 30;
 		h = 40;
-		rotor = 0.0f;
-		location = new PVector(300, 300);	
+		health = 50;
+		rotor_theta = 0.0f;
+		shadow_scale = 0.6f;
+		location = new PVector(300, 300);
+		objects = new ArrayList<GameObject>();
+		minim = new Minim(this.applet); // Required to use Minim.
+		snd_shoot = minim.loadFile("data/shoot.wav");
 	} // End Constructor.
+	
+	public void update() {
+		rotor_theta += 0.5f;
+		fire_bullet();
+		for (int i = 0; i < objects.size(); ++i) {
+			objects.get(i).run();
+			if (!objects.get(i).alive)
+				objects.remove(i);
+		} // End loop.
+	} // End update.
 	
 	public void display() {
 		applet.noStroke();
@@ -21,20 +44,17 @@ public class Helicopter extends GameObject {
 		draw_rotor_shadow();
 		draw_helicopter();
 		draw_rotor();
+		draw_health_bar();
 		applet.noFill();
+		//applet.noStroke();
 	} // End Display.
 	
-	public void update() {
-		rotor += 0.5f;
-	} // End update.
-	
 	private void draw_helicopter_shadow() {
-		// Draw the helicopter shadow.
-		applet.fill( 0, 0, 0, 120 );
 		applet.pushMatrix();
 		applet.translate( (location.x  + (w/2)) + 15, (location.y  + (h/2)) + 10);
 		applet.rotate(theta);
-		applet.scale(0.6f);
+		applet.scale(shadow_scale);
+		applet.fill( 0, 0, 0, 120 );
 		applet.rect(-15, h, 30, 3);    // Tail fin.
 		applet.rect(-3,h-20, 6, h-20); // Tail.
 		applet.ellipse(0,0, w, h);     // Body.
@@ -42,11 +62,10 @@ public class Helicopter extends GameObject {
 	} // End draw_helicopter_shadow.
 	
 	private void draw_rotor_shadow() {
-		// Draw the draw_rotor_shadow.
 		applet.pushMatrix();
 		applet.translate( (location.x  + (w/2)) + 15, (location.y  + (h/2)) + 10);
-		applet.rotate(rotor);
-		applet.scale(0.6f);
+		applet.rotate(rotor_theta);
+		applet.scale(shadow_scale);
 		applet.fill(0);
 		applet.rect(-1.5f, -40, 3, 80); // Vertical rotor.
 		applet.rect(-40, -1.5f, 80, 3); // Horizontal rotor.
@@ -54,11 +73,10 @@ public class Helicopter extends GameObject {
 	} // End draw_rotor_shadow.
 	
 	private void draw_helicopter() {
-		// Draw the helicopter.
-		applet.fill( colour.getRed(), colour.getGreen(), colour.getBlue(), colour.getAlpha() );
 		applet.pushMatrix();
 		applet.translate(location.x  + (w/2), location.y  + (h/2));
 		applet.rotate(theta);
+		applet.fill( colour.getRed(), colour.getGreen(), colour.getBlue(), colour.getAlpha() );
 		applet.rect(-15, h, 30, 3);    // Tail fin.
 		applet.rect(-3,h-20, 6, h-20); // Tail.
 		applet.ellipse(0,0, w, h);     // Body.
@@ -66,14 +84,30 @@ public class Helicopter extends GameObject {
 	} // End draw_helicopter.
 	
 	private void draw_rotor() {
-		// Draw the rotor.
 		applet.pushMatrix();
 		applet.translate(location.x  + (w/2), location.y  + (h/2));
-		applet.rotate(rotor);
+		applet.rotate(rotor_theta);
 		applet.fill(0);
 		applet.rect(-1.5f, -40, 3, 80); // Vertical rotor.
 		applet.rect(-40, -1.5f, 80, 3); // Horizontal rotor.
 		applet.popMatrix();
 	} // End draw_rotor.
+	
+	public void draw_health_bar() {
+		applet.pushMatrix();
+		applet.translate(location.x, location.y);
+		applet.fill(255, 0, 0, 255);
+		applet.rect(-health/3, 20, health, 5);
+		applet.popMatrix();
+	} // End draw_health_bar.
+	
+	public void fire_bullet() {
+		if(applet.millis() - timeSinceLastEvent >= 1000/fireRate){
+			snd_shoot.rewind();
+			snd_shoot.play();
 
+			timeSinceLastEvent = applet.millis();
+        }
+	} // End fire_bullet.
+	
 } // End Helicopter Class.
